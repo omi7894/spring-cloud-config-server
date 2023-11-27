06d9f6c3d58ae55e5092df8e549c22cecd57cf42
1136304 <omi7894@hyundai-autoever.com>
2023-11-27 15:50:21 +0900

    initial

---
+++ src/main/java/com/hae/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.hae.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
